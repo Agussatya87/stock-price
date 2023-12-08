e217388fa4bdb89bc360c1829266417c33353585
satya <95165186+Agussatya87@users.noreply.github.com>
2023-12-09 00:03:39 +0800

    stok_predict.py

--- stok_predict.py
+++ stok_predict.py
@@ -37,7 +37,13 @@ def plot_raw_data():
 	fig = go.Figure()
 	fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name="stock_open"))
 	fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name="stock_close"))
-	fig.layout.update(title_text='Time Series data with Rangeslider', xaxis_rangeslider_visible=True)
+	fig.layout.update(
+		title_text='Time Series data with Rangeslider',
+        	xaxis_rangeslider_visible=True,
+        	autosize=True,  # Set autosize to make the plot responsive
+        	margin=dict(l=0, r=0, b=0, t=40),  # Adjust margins for better visibility on mobile
+        	height=400,  # Set an initial height
+	)
 	st.plotly_chart(fig)
 	
 plot_raw_data()
