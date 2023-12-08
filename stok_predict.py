38c1ae28d2f82b432ad0ce3dd6a846dd7f54e443
satya <95165186+Agussatya87@users.noreply.github.com>
2023-12-08 23:44:29 +0800

    stok_predict.py

--- stok_predict.py
+++ stok_predict.py
@@ -1,20 +1,29 @@
 import streamlit as st
 from datetime import date
+
 import yfinance as yf
 from prophet import Prophet
 from prophet.plot import plot_plotly
 from plotly import graph_objs as go
+
+# Set page config for better mobile responsiveness
+st.set_page_config(
+    page_title="Prediksi Harga Saham",
+    page_icon=":chart_with_upwards_trend:",
+    layout="wide",
+)
 
 START = "2014-01-01"
 TODAY = date.today().strftime("%Y-%m-%d")
 
 st.title('Prediksi Harga Saham')
 
-stocks = ('BBRI.JK', 'BBCA.JK', 'BBNI.JK', 'BMRI.JK', 'BRIS.JK')
-selected_stock = st.selectbox('Select dataset for prediction', stocks)
-
-n_years = st.slider('Years of prediction:', 1, 5)
-period = n_years * 365
+# Wrap sliders in an expander for better mobile layout
+with st.expander("Options"):
+    stocks = ('BBRI.JK', 'BBCA.JK', 'BBNI.JK', 'BMRI.JK', 'BRIS.JK')
+    selected_stock = st.selectbox('Select dataset for prediction', stocks)
+    n_years = st.slider('Years of prediction:', 1, 5)
+    period = n_years * 365
 
 @st.cache
 def load_data(ticker):
@@ -26,30 +35,41 @@ data_load_state = st.text('Loading data...')
 data = load_data(selected_stock)
 data_load_state.text('Loading data... done!')
 
-# Raw data column
-raw_data_col, forecast_col = st.columns(2)
+st.subheader('Raw data')
+st.write(data.tail())
 
-with raw_data_col:
-    st.subheader('Raw data')
-    st.write(data.tail())
-    # Plot raw data
+# Inside the plot_raw_data() function, update the layout for better mobile responsiveness
+def plot_raw_data():
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name="stock_open"))
     fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name="stock_close"))
-    fig.layout.update(title_text='Time Series data with Rangeslider', xaxis_rangeslider_visible=True)
+    fig.layout.update(
+        title_text='Time Series data with Rangeslider',
+        xaxis_rangeslider_visible=True,
+        autosize=True,  # Enable autosize for better responsiveness
+        margin=dict(l=0, r=0, t=30, b=0),  # Adjust margin for better layout
+    )
     st.plotly_chart(fig)
 
-# Forecast column
-with forecast_col:
-    st.subheader('Forecast data')
-    st.write(forecast.tail())
-    
-    st.write(f'Forecast plot for {n_years} years')
-    fig1 = plot_plotly(m, forecast)
-    # Adjust plot dimensions for mobile
-    fig1.update_layout(height=400, width=600)
-    st.plotly_chart(fig1)
+plot_raw_data()
 
-    st.write("Forecast components")
-    fig2 = m.plot_components(forecast)
-    # Adjust plot dimensions for mobile
+# Predict forecast with Prophet.
+df_train = data[['Date','Close']]
+df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
+
+m = Prophet()
+m.fit(df_train)
+future = m.make_future_dataframe(periods=period)
+forecast = m.predict(future)
+
+# Show and plot forecast
+st.subheader('Forecast data')
+st.write(forecast.tail())
+    
+st.write(f'Forecast plot for {n_years} years')
+fig1 = plot_plotly(m, forecast)
+st.plotly_chart(fig1)
+
+st.write("Forecast components")
+fig2 = m.plot_components(forecast)
+st.write(fig2)
